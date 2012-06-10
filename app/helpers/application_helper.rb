05755edbdf9077c6c53aac1f5539d0f4344e036f
Eric Tiongson <eric.tiongson@gmail.com>
2012-06-10 23:42:26 +0800

    added blueprint css

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+  # returns a title on a per page basis
+  def title
+    base_title = "SelfServe"
+    if @title.nil?
+      base_title
+    else
+      "#{base_title} #{@title}"
+    end
+  end
 end
